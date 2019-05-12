f15a3fb3015861bce9f8dedb98d9616c556156a0
Max Maisel <max.maisel@posteo.de>
2019-05-12 11:38:41 +0200

    First working scheluder.

---
+++ src/scheduler.rs
@@ -0,0 +1,104 @@
+use std::cmp;
+use std::sync::{Arc, Condvar, Mutex};
+use std::time;
+
+struct ScheduledTask
+{
+    pending: bool,
+    id: u32,
+    next_wake: i64,
+    interval: i64
+}
+
+impl ScheduledTask
+{
+    fn new(id: u32, interval: i64) -> ScheduledTask
+    {
+        return ScheduledTask
+        {
+            pending: false,
+            id: id,
+            next_wake: 0,
+            interval: interval
+        };
+    }
+}
+
+pub struct Scheduler
+{
+    now: i64,
+    next_wake: i64,
+    tasks: Vec<ScheduledTask>
+}
+
+impl Scheduler
+{
+    pub fn new() -> Scheduler
+    {
+        return Scheduler
+        {
+            now: 0,
+            next_wake: 0,
+            tasks: Vec::new()
+        };
+    }
+
+    pub fn add_task(&mut self, id: u32, interval: i64)
+    {
+        self.tasks.push(ScheduledTask::new(id, interval));
+    }
+
+    fn check_pending_tasks(&mut self, shall_exec: bool)
+    {
+        self.next_wake = std::i64::MAX;
+        self.now = time::SystemTime::now().
+            duration_since(time::UNIX_EPOCH).
+            expect("💩️ Time outside of epoch").as_secs() as i64;
+
+        for task in self.tasks.iter_mut()
+        {
+            if task.next_wake <= self.now
+            {
+                task.next_wake =
+                    ((self.now / task.interval) + 1) * task.interval;
+                task.pending = shall_exec;
+            }
+            self.next_wake = cmp::min(task.next_wake, self.next_wake);
+        }
+    }
+
+    pub fn run<F>(&mut self, condition: Arc<(Mutex<bool>, Condvar)>, mut callback: F)
+        -> Result<(), String>
+        where F: FnMut(u32, i64) -> ()
+    {
+        println!("starting scheduler");
+        let &(ref mutex, ref cvar) = &*condition;
+        let mut running = mutex.lock().unwrap();
+
+        self.check_pending_tasks(false);
+        while !*running
+        {
+            self.check_pending_tasks(true);
+            for task in self.tasks.iter_mut()
+            {
+                if task.pending
+                {
+                    // TODO: add error handling for tasks?!
+                    callback(task.id, self.now);
+                    task.pending = false;
+                }
+            }
+
+            self.now = time::SystemTime::now().
+                duration_since(time::UNIX_EPOCH).
+                expect("💩️ Time outside of epoch").as_secs() as i64;
+            let delay = (self.next_wake - self.now) as u64;
+            println!("Sleep {} sec", delay);
+            let result = cvar.wait_timeout(running,
+                time::Duration::new(delay, 0)).expect("💩️ poisened mutex");
+            running = result.0;
+        }
+        println!("stopping scheduler");
+        return Ok(());
+    }
+}
