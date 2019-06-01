d61e9bf5bc0bff9469139dffe9676e5b9e07cbfb
Max Maisel <max.maisel@posteo.de>
2019-06-01 13:46:47 +0200

    Begin serialport related work.

--- lib/sml-client/lib.rs
+++ lib/sml-client/lib.rs
@@ -3,6 +3,8 @@ extern crate crc16;
 extern crate serialport;
 
 use std::io::Cursor;
+use std::time::Duration;
+use serialport::{SerialPort, SerialPortSettings};
 
 mod doc;
 use doc::SmlFile;
@@ -10,13 +12,30 @@ use doc::SmlBuf;
 
 pub struct SmlClient
 {
+    port: Box<SerialPort>
 }
 
 impl SmlClient
 {
-    pub fn decode(data: Vec<u8>) -> Result<SmlFile, String>
+    pub fn new(port_name: String, baudrate: u32, timeout: Duration)
+        -> Result<SmlClient, String>
     {
-        let mut buf = Cursor::new(data);
-        return SmlFile::deserialize(&mut buf);
+        let mut settings = SerialPortSettings::default();
+        settings.baud_rate = baudrate;
+        settings.timeout = timeout;
+        let port = match serialport::open_with_settings(&port_name, &settings)
+        {
+            Ok(port) => port,
+            Err(e) => return Err(format!(
+                "Failed to open {}, error: {}", port_name, e))
+        };
+
+        return Ok(SmlClient
+        {
+            port: port
+        });
     }
+
+    // TODO: get sml file from last two seconds or so
+    // TODO: extract data from file (to btreemap ?)
 }
