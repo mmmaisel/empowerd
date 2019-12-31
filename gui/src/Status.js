305180b0e628e39c6e17b7acc5a7a320b152c28c
Max Maisel <max.maisel@posteo.de>
2019-12-31 15:53:22 +0100

    Draw WaterSwitch widget dynamically.
    Also sort definitions.

--- gui/src/Status.js
+++ gui/src/Status.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
+import WaterSwitch from './WaterSwitch.js'
 
 class Status extends Component
 {
     render()
     {
+        // TODO: server time, manual trigger, next event
         return (
             <div>
                 Status page
+                <WaterSwitch count={6} states={[0, 1, 0, 1, 1, 0]}/>
             </div>
         );
     }
