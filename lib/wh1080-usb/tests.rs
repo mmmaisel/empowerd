7afced7e76d0c40d0611e278d1160a004857d996
Max Maisel <max.maisel@posteo.de>
2020-01-11 08:54:13 +0100

    Add read WH1080 HID data example.

--- lib/wh1080-usb/tests.rs
+++ lib/wh1080-usb/tests.rs
@@ -18,8 +18,22 @@ fn with_hid() {
 
     println!("Opened Device");
 
-    let mut buffer = [0; 1024];
-    for _ in 0..10 {
+    let mut buffer = [0; 32];
+    for addr in 0..32 {
+        let addr_h = ((addr as u16)*32/256) as u8;
+        let addr_l = ((addr as u16)*32%256) as u8;
+        let read_0_cmd = vec![0x00, 0xA1, addr_h, addr_l,
+            0x20, 0xA1, addr_h, addr_l, 0x20];
+        println!("Reading address {:X}", addr*32);
+        match device.write(&read_0_cmd) {
+            Ok(count) => {
+                if count != read_0_cmd.len() {
+                    panic!("Not all data was written");
+                }
+            }
+            Err(e) => panic!("Writing data failed: {}", e)
+        }
+
         match device.read(&mut buffer) {
             Ok(x) => println!("Received {} bytes: {:02X?}", x, &buffer[0..x]),
             Err(e) => panic!("Error reading device: {}", e)
