0d692f8303d2fb090060f2f4b2eee4859b617014
Max Maisel <max.maisel@posteo.de>
2020-01-12 17:19:32 +0100

    Manually decode Bresser PC 6 in 1 USB Protocol.

--- lib/wh1080-usb/tests.rs
+++ lib/wh1080-usb/tests.rs
@@ -2,7 +2,7 @@ extern crate hidapi;
 
 use hidapi::HidApi;
 
-//#[test]
+#[test]
 fn with_hid() {
     let api = match HidApi::new() {
         Ok(x) => x,
@@ -18,24 +18,29 @@ fn with_hid() {
 
     println!("Opened Device");
 
-    let mut buffer = [0; 32];
-    for addr in 0..32 {
-        let addr_h = ((addr as u16) * 32 / 256) as u8;
-        let addr_l = ((addr as u16) * 32 % 256) as u8;
-        let read_0_cmd =
-            vec![0x00, 0xA1, addr_h, addr_l, 0x20, 0xA1, addr_h, addr_l, 0x20];
-        println!("Reading address {:X}", addr * 32);
-        match device.write(&read_0_cmd) {
-            Ok(count) => {
-                if count != read_0_cmd.len() {
-                    panic!("Not all data was written");
-                }
+    let mut buffer = [0; 256];
+    /*let read_0_cmd =
+        vec![0x00, 0xfc, 0x03, 0x00, 0x00, 0x00, 0x2f, 0xa1, 0xfd]; // ???
+        //vec![0x00, 0xfc, 0x07, 0x00, 0x00, 0x00, 0xe5, 0x50, 0xfd]; // ???
+        vec![0x00, 0xfc, 0x08, 0x14, 0x01, 0x0c, 0xa0, 0x5c, 0xfd]; // ???
+        //vec![0x00, 0xfc, 0x09, 0x0d, 0x17, 0x24, 0x59, 0xfb, 0xfd]; // more output ?
+        //vec![0x00, 0xfc, 0xd4, 0x01, 0x00, 0x00, 0xe1, 0xbf, 0xfd]; // more output ?
+        //vec![0x00, 0xfc, 0xd5, 0x01, 0x00, 0x00, 0x97, 0x0b, 0xfd]; // more output ?
+    match device.write(&read_0_cmd) {
+        Ok(count) => {
+	    if count != read_0_cmd.len() {
+	        panic!("Not all data was written");
             }
-            Err(e) => panic!("Writing data failed: {}", e),
         }
+        Err(e) => panic!("Writing data failed: {}", e),
+    }*/
+    for addr in 0..16 {
 
         match device.read(&mut buffer) {
-            Ok(x) => println!("Received {} bytes: {:02X?}", x, &buffer[0..x]),
+            Ok(x) => {
+		println!("Received {} bytes: {}", x, String::from_utf8_lossy(&buffer[0..x]));
+		println!("{:2X?}", &buffer[0..x]);
+            }
             Err(e) => panic!("Error reading device: {}", e),
         }
     }
@@ -56,7 +61,7 @@ struct Endpoint {
     address: u8,
 }
 
-#[test]
+//#[test]
 fn with_rusb() {
     let vid = 0x1941;
     let pid = 0x8021;
@@ -256,7 +261,7 @@ fn configure_endpoint<T: UsbContext>(
     Ok(())
 }
 
-#[test]
+//#[test]
 fn decode_data() {
     let data = vec![
         0xFA, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -308,6 +313,44 @@ fn decode_data() {
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x43, 0x2F, 0xFD,
     ];
+
+    //@@þ1 6 1 2020-01-12 17:03 21.7 55 2.9 76 0.254 0.0 0.0 0.0 13 fý
+    //@@þ2 6 5 SE 1023 960 0 -0.9 --.- --.- -- --.- -- --.- -- --.- ¯ÿý
+    //@@þ3 . -- --.- -- --.- -- --.- -- wÛý
+
+    //          date      time  t_in hum_in t_out hum_out  rain_d  rain_a  wind_a wind_g wind_d..
+    //@@þ1 6 1 2020-01-12 17:03 21.7 55      2.9    76      0.254    0.0    0.0    0.0   13       fý
+    //       ..dir wind baro_sea baro_abs  uv?  dew
+    //@@þ2 6   5    SE     1023     960      0   -0.9 --.- --.- -- --.- -- --.- -- --.- ¯ÿý
+    //@@þ3 . -- --.- -- --.- -- --.- -- wÛý
+
+    //@@ûA 6 &dateutc=now&baromin=30.21&tempf=37.2&humidity=76&wind ".ý
+    //@@ûB 6 speedmph=0&windgustmph=0&winddir=135&dewptf=30.3&raini ¶Æý
+    //@@ûC 6 n=0&dailyrainin=0.01&UV=0&indoortempf=71.0&indoorhumid _0ý
+    //@@ûD   ity=55ý
+
+    //2020-01-12 13:24 21.1 52 5.6 65 0.0.0 2.5 2.5 16 S 1024 961 0 -0.5
+    //--.- --.- -- --.- -- --.- -- --.- 3-- --.- -- --.- -- --.- --
+    //&dateutc=now&baromin=30.24&tempf=42.0&humidity=65&windspeedmph=1.5
+    //&windgustmph=1.5&winddir=169&dewptf=31.1&rainin=0&dailyrainin=0.01
+    //&UV=0&indoortempf=69.9&indoorhumidity=52
+
+    // dateutc=now        [actual]
+    // &baromin=30.24     [inHG]    ==  1008 hPa    [hPa]  =  [inHg] /0.030
+    // &temf=42.0         [F]       ==  5,5  °C     [C]    =  5/9 *([F] - 32)
+    // &humidity=65       [%]
+    // &windspeedmph=1.5  [mph]     ==  2.41 km/h   [km/h] =  [mph] * 0.44704
+    // &windgustmph=1.5   [mph]     ==  2.41 km/h
+    // &winddir=169       [deg]
+    // &dewptf= 31.1      [F]       ==  -0,5 °C     [C]    =  5/9  * ([F] - 32)
+    // &rainin=0.01       [Inch]    ==  0.25 mm     [mm]   =  [in] *  25.40
+    // &UV= 0             [Index]
+    // &indoortempf=69.9  [F]       ==  21,05°C
+    // &indoorhumidity=52 [%]
+
+
+    println!("data1: {}", String::from_utf8_lossy(&data));
+    println!("data2: {}", String::from_utf8_lossy(&data2));
 
     println!("data len: {}", data.len());
     println!("data2 len: {}", data2.len());
