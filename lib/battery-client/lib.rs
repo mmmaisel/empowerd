ec03fa7fad3ae99fefbb00ac2f81f13bbbf5d12c
Max Maisel <max.maisel@posteo.de>
2020-09-10 11:19:05 +0200

    Improve battery-client error handling.

--- lib/battery-client/lib.rs
+++ lib/battery-client/lib.rs
@@ -29,29 +29,48 @@ impl BatteryClient {
         };
     }
 
+    fn validate_result(
+        &self,
+        which: &str,
+        res: Result<Vec<u16>, modbus::Error>,
+    ) -> Result<u32, String> {
+        match res {
+            Err(e) => return Err(e.to_string()),
+            Ok(data) => {
+                if let Some(l) = &self.logger {
+                    trace!(l, "RAW {}: {:?}", &which, &data);
+                }
+                if data.iter().all(|x| *x == 0xFFFF) {
+                    return Err(format!(
+                        "Received invalid value for {}",
+                        which
+                    ));
+                }
+                return Ok((data[0] as u32) * 65536 + (data[1] as u32));
+            }
+        };
+    }
+
     pub fn get_in_out_charge(&self) -> Result<(u32, u32, u32), String> {
         let mut client =
             modbus::tcp::Transport::new_with_cfg(&self.addr, self.config)
                 .map_err(|e| e.to_string())?;
-        let raw_wh_in = client
-            .read_input_registers(BatteryClient::METERING_WH_IN, 2)
-            .map_err(|e| e.to_string())?;
-        let raw_wh_out = client
-            .read_input_registers(BatteryClient::METERING_WH_OUT, 2)
-            .map_err(|e| e.to_string())?;
-        let raw_charge = client
-            .read_input_registers(BatteryClient::BAT_CHA_STT, 2)
-            .map_err(|e| e.to_string())?;
+        let wh_in = self.validate_result(
+            "METERING_WH_IN",
+            client.read_input_registers(BatteryClient::METERING_WH_IN, 2),
+        )?;
+        let wh_out = self.validate_result(
+            "METERING_WH_OUT",
+            client.read_input_registers(BatteryClient::METERING_WH_OUT, 2),
+        )?;
+        let charge = self.validate_result(
+            "BAT_CHA_STT",
+            client.read_input_registers(BatteryClient::BAT_CHA_STT, 2),
+        )?;
 
-        if let Some(l) = &self.logger {
-            trace!(l, "raw_wh_in: {:?}", &raw_wh_in);
-            trace!(l, "raw_wh_out: {:?}", &raw_wh_out);
-            trace!(l, "raw_charge: {:?}", &raw_charge);
+        if charge == 0 {
+            return Err("Received invalid value 0 for charge.".into());
         }
-
-        let wh_in = (raw_wh_in[0] as u32) * 65536 + (raw_wh_in[1] as u32);
-        let wh_out = (raw_wh_out[0] as u32) * 65536 + (raw_wh_out[1] as u32);
-        let charge = (raw_charge[0] as u32) * 65536 + (raw_charge[1] as u32);
 
         return Ok((wh_in, wh_out, charge));
     }
