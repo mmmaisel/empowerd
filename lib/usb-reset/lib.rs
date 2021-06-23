281e47dfc8f4b9cf8a98f58a0eeaddcbcf336ede
Max Maisel <max.maisel@posteo.de>
2021-06-23 22:19:46 +0200

    Add usb-reset library.
    This allows to issue a software reset to a nonresponsive USB device.
    It requires write access on the USB bus device node so add a udev rule
    for the current meter USB TTY device.
    Put the reset code into its own crate to allow using "forbid_unsafe" in the
    main code.

---
+++ lib/usb-reset/lib.rs
@@ -0,0 +1,62 @@
+use nix::ioctl_none;
+use std::{
+    ffi::OsStr,
+    fs::{File, OpenOptions},
+    os::unix::io::AsRawFd,
+    path::Path,
+};
+use udev::{Device, Enumerator};
+
+// see /usr/include/linux/usbdevice_fs.h
+const USBDEVFS_MAGIC: u8 = b'U';
+const USBDEVFS_RESET: u8 = 20;
+
+ioctl_none!(usbdevfs_reset, USBDEVFS_MAGIC, USBDEVFS_RESET);
+
+pub fn reset_device(path: String) -> Result<(), String> {
+    let mut enumerator = Enumerator::new().map_err(|e| e.to_string())?;
+
+    let mut device = enumerator
+        .scan_devices()
+        .map_err(|e| e.to_string())?
+        .find(|device| {
+            if let Some(devnode) = device.devnode() {
+                if let Some(devpath) = devnode.to_str() {
+                    devpath == path
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        });
+
+    while let Some(dev) = device {
+        if let Some(driver) = dev.driver() {
+            if driver == "usb" {
+                let devnode = match dev.devnode() {
+                    Some(x) => x,
+                    None => {
+                        return Err(format!("USB parent device has no node"))
+                    }
+                };
+
+                return match OpenOptions::new().write(true).open(&devnode) {
+                    Ok(devfile) => unsafe {
+                        if let Err(e) = usbdevfs_reset(devfile.as_raw_fd()) {
+                            Err(format!("Reset USB device failed: {}", e))
+                        } else {
+                            Ok(())
+                        }
+                    },
+                    Err(e) => {
+                        Err(format!("Could not open USB device node: {}", e))
+                    }
+                };
+            }
+        }
+        device = dev.parent();
+    }
+
+    return Err("No parent USB device found".into());
+}
