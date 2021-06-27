91b55bd3ceaace4760a2fc6e9a872a586e464909
Max Maisel <max.maisel@posteo.de>
2021-06-27 11:00:54 +0200

    Improve usb-reset error messages.

--- lib/usb-reset/lib.rs
+++ lib/usb-reset/lib.rs
@@ -9,50 +9,51 @@ const USBDEVFS_RESET: u8 = 20;
 ioctl_none!(usbdevfs_reset, USBDEVFS_MAGIC, USBDEVFS_RESET);
 
 pub fn reset_path(path: &str) -> Result<(), String> {
-    let mut enumerator = Enumerator::new().map_err(|e| e.to_string())?;
-
-    let device =
-        enumerator
-            .scan_devices()
-            .map_err(|e| e.to_string())?
-            .find(|device| {
-                if let Some(devnode) = device.devnode() {
-                    if let Some(devpath) = devnode.to_str() {
-                        devpath == path
-                    } else {
-                        false
-                    }
-                } else {
-                    false
-                }
-            });
+    let device = find_device(|device| {
+        if let Some(devnode) = device.devnode() {
+            if let Some(devpath) = devnode.to_str() {
+                devpath == path
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    })?;
 
     return reset_device(device);
 }
 
 pub fn reset_vid_pid(vid: u16, pid: u16) -> Result<(), String> {
-    let mut enumerator = Enumerator::new().map_err(|e| e.to_string())?;
     let vid = format!("{:04x}", vid);
     let pid = format!("{:04x}", pid);
 
-    let device =
-        enumerator
-            .scan_devices()
-            .map_err(|e| e.to_string())?
-            .find(|device| {
-                if let Some(dvid) = device.property_value("ID_VENDOR_ID") {
-                    if let Some(dpid) = device.property_value("ID_MODEL_ID") {
-                        dvid.to_string_lossy() == vid
-                            && dpid.to_string_lossy() == pid
-                    } else {
-                        false
-                    }
-                } else {
-                    false
-                }
-            });
+    let device = find_device(|device| {
+        if let Some(dvid) = device.property_value("ID_VENDOR_ID") {
+            if let Some(dpid) = device.property_value("ID_MODEL_ID") {
+                dvid.to_string_lossy() == vid && dpid.to_string_lossy() == pid
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    })?;
 
     return reset_device(device);
+}
+
+fn find_device<F: Fn(&Device) -> bool>(
+    cb: F,
+) -> Result<Option<Device>, String> {
+    let mut enumerator =
+        Enumerator::new().map_err(|e| format!("Enumerate failed: {}", e))?;
+
+    let device = enumerator
+        .scan_devices()
+        .map_err(|e| format!("Scan devices failed: {}", e))?
+        .find(cb);
+    return Ok(device);
 }
 
 fn reset_device(mut device: Option<Device>) -> Result<(), String> {
