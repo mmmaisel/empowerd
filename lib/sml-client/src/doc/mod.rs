fac1fbe01144dcd50ff29d4e9eda90af3a757e6c
Max Maisel <max.maisel@posteo.de>
2024-02-03 11:19:38 +0100

    Remove unnecessary use statements in sml_client.

--- lib/sml-client/src/doc/mod.rs
+++ lib/sml-client/src/doc/mod.rs
@@ -24,14 +24,10 @@ mod sml_open;
 mod sml_stream;
 mod sml_types;
 
-pub use sml_buffer::*;
-pub use sml_close::*;
 pub use sml_file::*;
 pub use sml_get_list::*;
 pub use sml_message::*;
-pub use sml_open::*;
 pub use sml_stream::*;
-pub use sml_types::*;
 
 #[cfg(test)]
 mod tests;
