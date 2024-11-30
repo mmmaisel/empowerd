1bc43bf358be556ae0f0e6bd975bd497d03727df
Max Maisel <max.maisel@posteo.de>
2024-11-30 14:06:13 +0100

    Add Barometer and Wind to Weather scene.

--- grafana-gui/src/Weather.tsx
+++ grafana-gui/src/Weather.tsx
@@ -9,10 +9,12 @@ import {
     SceneToolbarButton,
 } from "@grafana/scenes";
 
+import { BaroPlot } from "./panels/BaroPlot";
 import { ConfigJson } from "./AppConfig";
 import { HumidityPlot } from "./panels/HumidityPlot";
 import { RainPlot } from "./panels/RainPlot";
 import { TemperaturePlot } from "./panels/TemperaturePlot";
+import { WindPlot } from "./panels/WindPlot";
 
 // TODO: dedup controls and embedded scene
 export const WeatherScene = (
@@ -22,23 +24,45 @@ export const WeatherScene = (
     let hums = HumidityPlot(config);
     let temps = TemperaturePlot(config);
     let rain = RainPlot(config);
+    let baro = BaroPlot(config);
+    let wind = WindPlot(config);
     return new EmbeddedScene({
         $timeRange: new SceneTimeRange({ from: "now-2d", to: "now" }),
         body: new SceneCSSGridLayout({
             templateColumns: "1fr 1fr",
-            templateRows: "3fr 3fr 1fr",
+            templateRows: "1fr",
             children: [
-                new EmbeddedScene({
-                    $data: temps.query,
-                    body: temps.scene,
+                new SceneCSSGridLayout({
+                    templateColumns: "1fr",
+                    templateRows: "1fr 1fr",
+                    children: [
+                        new EmbeddedScene({
+                            $data: temps.query,
+                            body: temps.scene,
+                        }),
+                        new EmbeddedScene({
+                            $data: hums.query,
+                            body: hums.scene,
+                        }),
+                    ],
                 }),
-                new EmbeddedScene({
-                    $data: rain.query,
-                    body: rain.scene,
-                }),
-                new EmbeddedScene({
-                    $data: hums.query,
-                    body: hums.scene,
+                new SceneCSSGridLayout({
+                    templateColumns: "1fr",
+                    templateRows: "1fr 1fr 1fr",
+                    children: [
+                        new EmbeddedScene({
+                            $data: rain.query,
+                            body: rain.scene,
+                        }),
+                        new EmbeddedScene({
+                            $data: baro.query,
+                            body: baro.scene,
+                        }),
+                        new EmbeddedScene({
+                            $data: wind.query,
+                            body: wind.scene,
+                        }),
+                    ],
                 }),
             ],
         }),
