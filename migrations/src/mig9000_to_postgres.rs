ba73e2c1627aacd85d7b735ac6d607c342d67e84
Max Maisel <max.maisel@posteo.de>
2025-05-28 17:59:49 +0200

    Move weather schema into migration 9000.

--- migrations/src/mig9000_to_postgres.rs
+++ migrations/src/mig9000_to_postgres.rs
@@ -32,7 +32,6 @@ use libempowerd::{
         postgres::{
             run_migrations, Battery as PgBattery, BidirMeter as PgBidirMeter,
             Generator as PgGenerator, SimpleMeter as PgSimpleMeter,
-            Weather as PgWeather,
         },
         units::second,
     },
@@ -47,8 +46,10 @@ mod schema_9000 {
     use libempowerd::{
         error::Error,
         models::units::{
-            celsius, percent, second, watt, watt_hour, Abbreviation, Energy,
-            Power, Ratio, Temperature, Time,
+            celsius, degree, micrometer, millimeter_per_second, pascal,
+            percent, ratio, second, watt, watt_hour, Abbreviation, Angle,
+            Energy, Length, Power, Pressure, Ratio, Temperature, Time,
+            Velocity,
         },
     };
     use std::convert::TryFrom;
@@ -158,9 +159,182 @@ mod schema_9000 {
             })
         }
     }
+
+    diesel::table! {
+        weathers (series_id, time) {
+            series_id -> Int4,
+            time -> Timestamp,
+            temp_in_degc_e1 -> Int2,
+            hum_in_e3 -> Int2,
+            temp_out_degc_e1 -> Nullable<Int2>,
+            hum_out_e3 -> Nullable<Int2>,
+            rain_day_um ->Int4,
+            rain_act_um -> Int4,
+            wind_act_mms -> Int4,
+            wind_gust_mms -> Int4,
+            wind_dir_deg_e1 -> Int2,
+            baro_sea_pa -> Int4,
+            baro_abs_pa -> Int4,
+            uv_index_e1 -> Int2,
+            dew_point_degc_e1 -> Nullable<Int2>,
+            temp_x1_degc_e1 -> Nullable<Int2>,
+            hum_x1_e3 -> Nullable<Int2>,
+            temp_x2_degc_e1 -> Nullable<Int2>,
+            hum_x2_e3 -> Nullable<Int2>,
+            temp_x3_degc_e1 -> Nullable<Int2>,
+            hum_x3_e3 -> Nullable<Int2>,
+        }
+    }
+
+    #[derive(AsChangeset, Identifiable, Insertable, Queryable, Selectable)]
+    #[diesel(table_name = weathers)]
+    #[diesel(check_for_backend(diesel::pg::Pg))]
+    #[diesel(primary_key(series_id, time))]
+    pub struct RawPgWeather {
+        pub series_id: i32,
+        pub time: NaiveDateTime,
+        pub temp_in_degc_e1: i16,
+        pub hum_in_e3: i16,
+        pub temp_out_degc_e1: Option<i16>,
+        pub hum_out_e3: Option<i16>,
+        pub rain_day_um: i32,
+        pub rain_act_um: i32,
+        pub wind_act_mms: i32,
+        pub wind_gust_mms: i32,
+        pub wind_dir_deg_e1: i16,
+        pub baro_sea_pa: i32,
+        pub baro_abs_pa: i32,
+        pub uv_index_e1: i16,
+        pub dew_point_degc_e1: Option<i16>,
+        pub temp_x1_degc_e1: Option<i16>,
+        pub hum_x1_e3: Option<i16>,
+        pub temp_x2_degc_e1: Option<i16>,
+        pub hum_x2_e3: Option<i16>,
+        pub temp_x3_degc_e1: Option<i16>,
+        pub hum_x3_e3: Option<i16>,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct PgWeather {
+        pub time: Time,
+        pub temp_in: Temperature,
+        pub hum_in: Ratio,
+        pub temp_out: Option<Temperature>,
+        pub hum_out: Option<Ratio>,
+        pub rain_day: Length,
+        pub rain_act: Length,
+        pub wind_act: Velocity,
+        pub wind_gust: Velocity,
+        pub wind_dir: Angle,
+        pub baro_sea: Pressure,
+        pub baro_abs: Pressure,
+        pub uv_index: Ratio,
+        pub dew_point: Option<Temperature>,
+        pub temp_x1: Option<Temperature>,
+        pub hum_x1: Option<Ratio>,
+        pub temp_x2: Option<Temperature>,
+        pub hum_x2: Option<Ratio>,
+        pub temp_x3: Option<Temperature>,
+        pub hum_x3: Option<Ratio>,
+    }
+
+    impl PgWeather {
+        pub async fn insert_bulk(
+            data: Vec<Self>,
+            conn: &mut diesel_async::AsyncPgConnection,
+            series_id: i32,
+        ) -> Result<usize, super::Error> {
+            use diesel_async::RunQueryDsl;
+            let raw = data
+                .iter()
+                .map(|x| {
+                    let mut y = RawPgWeather::try_from(x)?;
+                    y.series_id = series_id;
+                    Ok(y)
+                })
+                .collect::<Result<Vec<RawPgWeather>, super::Error>>()?;
+
+            diesel::insert_into(weathers::table)
+                .values(&raw)
+                .on_conflict_do_nothing()
+                .execute(conn)
+                .await
+                .map_err(|e| {
+                    Error::Temporary(format!(
+                        "Inserting record into series {series_id} failed: {e}",
+                    ))
+                })
+        }
+    }
+
+    impl TryFrom<&PgWeather> for RawPgWeather {
+        type Error = Error;
+        fn try_from(input: &PgWeather) -> Result<Self, Self::Error> {
+            Ok(Self {
+                series_id: 0,
+                time: DateTime::from_timestamp(
+                    input.time.get::<second>() as i64,
+                    0,
+                )
+                .ok_or_else(|| {
+                    Error::InvalidInput(format!(
+                        "Invalid timestamp: {:?}",
+                        input.time.into_format_args(second, Abbreviation),
+                    ))
+                })?
+                .naive_utc(),
+                temp_in_degc_e1: (input.temp_in.get::<celsius>() * 1e1).round()
+                    as i16,
+                hum_in_e3: (input.hum_in.get::<percent>() * 1e1).round() as i16,
+                temp_out_degc_e1: input
+                    .temp_out
+                    .map(|x| (x.get::<celsius>() * 1e1).round() as i16),
+                hum_out_e3: input
+                    .hum_out
+                    .map(|x| (x.get::<percent>() * 1e1).round() as i16),
+                rain_day_um: input.rain_day.get::<micrometer>().round() as i32,
+                rain_act_um: input.rain_act.get::<micrometer>().round() as i32,
+                wind_act_mms: input
+                    .wind_act
+                    .get::<millimeter_per_second>()
+                    .round() as i32,
+                wind_gust_mms: input
+                    .wind_gust
+                    .get::<millimeter_per_second>()
+                    .round() as i32,
+                wind_dir_deg_e1: (input.wind_dir.get::<degree>() * 1e1).round()
+                    as i16,
+                baro_sea_pa: input.baro_sea.get::<pascal>().round() as i32,
+                baro_abs_pa: input.baro_abs.get::<pascal>().round() as i32,
+                uv_index_e1: (input.uv_index.get::<ratio>() * 1e1).round()
+                    as i16,
+                dew_point_degc_e1: input
+                    .dew_point
+                    .map(|x| (x.get::<celsius>() * 1e1).round() as i16),
+                temp_x1_degc_e1: input
+                    .temp_x1
+                    .map(|x| (x.get::<celsius>() * 1e1).round() as i16),
+                hum_x1_e3: input
+                    .hum_x1
+                    .map(|x| (x.get::<percent>() * 1e1).round() as i16),
+                temp_x2_degc_e1: input
+                    .temp_x2
+                    .map(|x| (x.get::<celsius>() * 1e1).round() as i16),
+                hum_x2_e3: input
+                    .hum_x2
+                    .map(|x| (x.get::<percent>() * 1e1).round() as i16),
+                temp_x3_degc_e1: input
+                    .temp_x3
+                    .map(|x| (x.get::<celsius>() * 1e1).round() as i16),
+                hum_x3_e3: input
+                    .hum_x3
+                    .map(|x| (x.get::<percent>() * 1e1).round() as i16),
+            })
+        }
+    }
 }
 
-use schema_9000::PgHeatpump;
+use schema_9000::{PgHeatpump, PgWeather};
 
 #[derive(Args, Clone, Debug)]
 pub struct Mig9000Args {
@@ -496,14 +670,14 @@ async fn migrate_weather(
                     hum_in: record.humidity_in,
                     temp_out: record.temperature_out,
                     hum_out: record.humidity_out,
-                    rain_day: record.rain_day,
-                    rain_act: record.rain_actual,
-                    wind_act: record.wind_actual,
-                    wind_gust: record.wind_gust,
-                    wind_dir: record.wind_dir,
+                    rain_day: record.rain_day.unwrap_or_default(),
+                    rain_act: record.rain_actual.unwrap_or_default(),
+                    wind_act: record.wind_actual.unwrap_or_default(),
+                    wind_gust: record.wind_gust.unwrap_or_default(),
+                    wind_dir: record.wind_dir.unwrap_or_default(),
                     baro_sea: record.baro_sea,
                     baro_abs: record.baro_absolute,
-                    uv_index: record.uv_index,
+                    uv_index: record.uv_index.unwrap_or_default(),
                     dew_point: record.dew_point,
                     temp_x1: record.temperature_x1,
                     hum_x1: record.humidity_x1,
@@ -511,14 +685,6 @@ async fn migrate_weather(
                     hum_x2: record.humidity_x2,
                     temp_x3: record.temperature_x3,
                     hum_x3: record.humidity_x3,
-                    temp_x4: None,
-                    hum_x4: None,
-                    temp_x5: None,
-                    hum_x5: None,
-                    temp_x6: None,
-                    hum_x6: None,
-                    temp_x7: None,
-                    hum_x7: None,
                 }
             })
             .collect::<Vec<PgWeather>>();
